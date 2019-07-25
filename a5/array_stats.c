fca210a6459c596e9698af76c59b5a2f62458971
johnnyd <johnnyd@asb9838nu-e01.csil.sfu.ca>
2019-07-25 14:39:23 -0700

    V1.1.0

--- a5/array_stats.c
+++ a5/array_stats.c
@@ -1,5 +1,6 @@
 #include <linux/kernel.h>
-#include <limits.h>
+#include <linux/uaccess.h>
+#include <linux/errno.h>
 #include "array_stats.h"
 
 asmlinkage long sys_array_stats(
@@ -9,10 +10,14 @@ asmlinkage long sys_array_stats(
 
 asmlinkage long sys_array_stats(struct array_stats *stats, long data[], long size){
 	long sum = 0;
-	long max = LONG_MIN;
-	long min = LONG_MAX;
+	long max = 0;
+	long min = 0;
 	long buf = 0;
-	struct array_stats* stuff;
+	long i = 0;
+	struct array_stats* stuff = NULL;
+	stuff->sum = 0;
+	stuff->max = 0;
+	stuff->min = 0;
 	if (size <= 0)
 	{
 		printk("size is <= 0\n");
@@ -24,13 +29,23 @@ asmlinkage long sys_array_stats(struct array_stats *stats, long data[], long siz
 		printk("stats or data is NULL\n");
 		return -EFAULT;
 	}
-	for (long i = 0; i < size; ++i)
+	if (copy_from_user(&max, &data[0], sizeof(long)) != 0)
+	{
+		return -EFAULT;
+	}
+	if (copy_from_user(&min, &data[0], sizeof(long)) != 0)
+	{
+		return -EFAULT;
+	}
+	sum += max;
+	for (i = 1; i < size; ++i)
 	{
 		if (copy_from_user(&buf, &data[i], sizeof(long)) != 0)
 		{
 			printk("Failed to read data from user\n");
 			return -EFAULT;
 		}
+		printk("buf is %ld\n", buf);
 		sum += buf;
 		if (buf > max)
 		{
@@ -44,6 +59,9 @@ asmlinkage long sys_array_stats(struct array_stats *stats, long data[], long siz
 	stuff->sum = sum;
 	stuff->max = max;
 	stuff->min = min;
+	printk("Stuff->sum is %ld\n", stuff->sum);
+	printk("Stuff->max is %ld\n", stuff->max);
+	printk("Stuff->min is %ld\n", stuff->min);
 	if (copy_to_user(stats, stuff, sizeof(struct array_stats*)) != 0)
 	{
 		printk("Failed to write stats to user\n");
