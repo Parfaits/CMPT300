328b41d5fc1837f9a5b2b50c454322385e9fd76b
Parfaits <36318895+Parfaits@users.noreply.github.com>
2019-07-25 18:50:56 -0700

    Update array_stats.c

--- a5/array_stats.c
+++ a5/array_stats.c
@@ -12,7 +12,7 @@ asmlinkage long sys_array_stats(struct array_stats *stats, long data[], long siz
 	long sum = 0;
 	long max = 0;
 	long min = 0;
-	long buf = 0;
+	long buf[size];
 	struct array_stats test;
 	long i = 0;
 	struct array_stats stuff;
@@ -35,37 +35,22 @@ asmlinkage long sys_array_stats(struct array_stats *stats, long data[], long siz
 		printk("stats or data is NULL\n");
 		return -EFAULT;
 	}
-	if (copy_from_user(&max, &data[0], sizeof(long)) != 0)
+	if (copy_from_user(buf, data, size*sizeof(long)) != 0)
 	{
 		return -EFAULT;
 	}
-	if (copy_from_user(&min, &data[0], sizeof(long)) != 0)
+	printk("buf[0]=%ld\nn", buf[0]);
+	for (i = 0; i < size; ++i)
 	{
-		return -EFAULT;
-	}
-	sum += max;
-	for (i = 1; i < size; ++i)
-	{
-		if (copy_from_user(&buf, &data[i], sizeof(long)) != 0)
+		sum += buf[i];
+		if (buf[i] > max)
 		{
-			printk("Failed to read data from user\n");
-			return -EFAULT;
+			max = buf[i];
 		}
-		// printk("buf is %ld\n", buf);
-		sum += buf;
-		if (buf > max)
+		if (buf[i] < min)
 		{
-			max = buf;
+			min = buf[i];
 		}
-		if (buf < min)
-		{
-			min = buf;
-		}
-	}
-	if (size == 1)
-	{
-		stuff.max = buf;
-		stuff.min = buf;
 	}
 	printk("Stuff.sum is %ld\n", stuff.sum);
 	printk("Stuff.max is %ld\n", stuff.max);
