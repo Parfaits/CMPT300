c675192e5cf7ade245f6cf52bca211eee1bdd390
fchui <masil@live.ca>
2019-07-28 06:06:16 -0700

    PID, Names, State, nvcsw, nivcsw - Complete
    The Children handling needs work, as it is not counting that correctly.
    
    Variable Current, will give all the information of the current process in the form of task_struct. Thus on line 49 I made it equal task.
    If we did not do so, current = current->parent does not work as we cannot manipulate current. 
    
    Mostly changed the current variables into task.
    current == current->parent does not work since current will never change its value in a process.
    
    Needs work in counting children properly

--- a5/process_ancestors.c
+++ a5/process_ancestors.c
@@ -3,6 +3,7 @@
 #include <linux/errno.h>
 #include <linux/sched.h>
 #include <linux/cred.h>
+
 #include "process_ancestors.h"
 
 asmlinkage long sys_process_ancestors(
@@ -14,10 +15,11 @@ asmlinkage long sys_process_ancestors(struct process_info info_array[], long siz
 	long filled = 0;
 	long i = 0;
 	long j = 0;
-	long count_children = 0;
-	long count_sibling = 0;
+	//long count_children = 0;
+	//long count_sibling = 0;
 	struct process_info info;
 	struct task_struct *task;
+	//struct task_struct *child_struct;
 	struct list_head *head_children;
 	struct list_head *head_sibling;
 	printk("size is %ld\n", size);
@@ -26,6 +28,8 @@ asmlinkage long sys_process_ancestors(struct process_info info_array[], long siz
 		printk("size was 0\n");
 		return -EINVAL;
 	}
+
+
 	if (copy_from_user(&filled, num_filled, sizeof(long)) != 0)
 	{
 		printk("Error copying num_filled\n");
@@ -41,52 +45,63 @@ asmlinkage long sys_process_ancestors(struct process_info info_array[], long siz
 		printk("Error copying current process\n");
 		return -EFAULT;
 	}
+
+	task = current;
+	filled++;
+	printk("current->pid is %d", current->pid);
 	for (i = 0; i < size; ++i)
+	//while (current->parent != current)
 	{
 		if (copy_from_user(&info, &info_array[i], sizeof(struct process_info)) != 0)
 		{
 			printk("Error copying info_array[%ld]\n", i);
-			return -EFAULT;
+			//return -EFAULT;
 		}
-		info.pid = current->pid;
+
+		info.pid = task->pid;
 		for (j = 0; j < ANCESTOR_NAME_LEN; ++j)
 		{
-			info.name[j] = current->comm[j];
+			info.name[j] = task->comm[j];
 		}
-		info.state = current->state;
-		info.uid =  current->cred->uid.val;
-		info.nvcsw = current->nvcsw;
-		info.nivcsw = current->nivcsw;
-		list_for_each(head_children, &current->children){
-			info.num_children++;
-			task = list_entry(head_children, struct task_struct, children);
-			
-		}
-		list_for_each(head_sibling, &current->sibling){
-			info.num_siblings++;
-			task = list_entry(head_sibling, struct task_struct, sibling);
-		}
-		info.num_children = count_children;
-		info.num_siblings = count_sibling;
+		info.state = task->state;
+		info.uid =  task->cred->uid.val;
+		info.nvcsw = task->nvcsw;
+		info.nivcsw = task->nivcsw;
 		printk("info.pid is %ld", info.pid);
+		printk("info.name is %s", info.name);
 		printk("info.state is %ld", info.state);
-		printk("info.uid is %ld", info.uid);
 		printk("info.nvcsw is %ld", info.nvcsw);
 		printk("info.nivcsw is %ld", info.nivcsw);
-		printk("info.num_children is %ld", info.num_children);
-		printk("info.num_siblings is %ld", info.num_siblings);
-		filled++;
+	
+		list_for_each(head_children, &task->children){
+			info.num_children++;
+			//task = list_entry(head_children, struct task_struct, children);		
+		}
+		list_for_each(head_sibling, &task->sibling){
+			info.num_siblings++;
+			//task = list_entry(head_sibling, struct task_struct, sibling);
+		}
+
 		if (copy_to_user(&info_array[i], &info, sizeof(struct process_info)) != 0)
 		{
 			printk("Error copying info to user\n");
 			return -EFAULT;
 		}
-		task = current->parent;
+
+		filled++;
+
+		if (task->parent->pid == info.pid)
+		{
+			break;
+		}
+		task = task->parent;
+
 	}
 	if (copy_to_user(num_filled, &filled, sizeof(long)) != 0)
 	{
 		printk("Error copying filled to user\n");
 		return -EFAULT;
 	}
+	
 	return 0;
 }
