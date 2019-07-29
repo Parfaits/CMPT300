edf8ff3c90cf202702ada2821ca23cbe50db50a2
Parfaits <36318895+Parfaits@users.noreply.github.com>
2019-07-29 15:12:31 -0700

    fixed num_filled
    Update how it iterates through children/sibling list, patched how it iterates through the ancestors; now it iterates through properly. Still in need of testing: Children/Sibling count of each process, any other test cases if applicable.

--- a5/process_ancestors.c
+++ a5/process_ancestors.c
@@ -3,7 +3,6 @@
 #include <linux/errno.h>
 #include <linux/sched.h>
 #include <linux/cred.h>
-
 #include "process_ancestors.h"
 
 asmlinkage long sys_process_ancestors(
@@ -12,14 +11,15 @@ asmlinkage long sys_process_ancestors(
                 long *num_filled);
 
 asmlinkage long sys_process_ancestors(struct process_info info_array[], long size, long *num_filled){
+	long check_num_filled = 0;
 	long filled = 0;
 	long i = 0;
 	long j = 0;
-	//long count_children = 0;
-	//long count_sibling = 0;
+	long count_children = 0;
+	long count_sibling = 0;
 	struct process_info info;
-	struct task_struct *task;
-	//struct task_struct *child_struct;
+	struct task_struct *task = current;
+	struct task_struct *temp = NULL;
 	struct list_head *head_children;
 	struct list_head *head_sibling;
 	printk("size is %ld\n", size);
@@ -28,9 +28,7 @@ asmlinkage long sys_process_ancestors(struct process_info info_array[], long siz
 		printk("size was 0\n");
 		return -EINVAL;
 	}
-
-
-	if (copy_from_user(&filled, num_filled, sizeof(long)) != 0)
+	if (copy_from_user(&check_num_filled, num_filled, sizeof(long)) != 0)
 	{
 		printk("Error copying num_filled\n");
 		return -EFAULT;
@@ -40,68 +38,62 @@ asmlinkage long sys_process_ancestors(struct process_info info_array[], long siz
 		printk("info_array or num_filled was NULL\n");
 		return -EFAULT;
 	}
-	if (copy_from_user(&info, info_array, sizeof(struct process_info)) != 0)
-	{
-		printk("Error copying current process\n");
-		return -EFAULT;
-	}
-
-	task = current;
-	filled++;
-	printk("current->pid is %d", current->pid);
+	printk("num_filled is %ld\n", check_num_filled);
 	for (i = 0; i < size; ++i)
-	//while (current->parent != current)
 	{
 		if (copy_from_user(&info, &info_array[i], sizeof(struct process_info)) != 0)
 		{
 			printk("Error copying info_array[%ld]\n", i);
-			//return -EFAULT;
+			return -EFAULT;
 		}
-
 		info.pid = task->pid;
 		for (j = 0; j < ANCESTOR_NAME_LEN; ++j)
 		{
 			info.name[j] = task->comm[j];
 		}
+		j = 0;
 		info.state = task->state;
 		info.uid =  task->cred->uid.val;
 		info.nvcsw = task->nvcsw;
 		info.nivcsw = task->nivcsw;
-		printk("info.pid is %ld", info.pid);
-		printk("info.name is %s", info.name);
-		printk("info.state is %ld", info.state);
-		printk("info.nvcsw is %ld", info.nvcsw);
-		printk("info.nivcsw is %ld", info.nivcsw);
-	
-		list_for_each(head_children, &task->children){
+		list_for_each(head_children, &current->children){
 			info.num_children++;
-			//task = list_entry(head_children, struct task_struct, children);		
+			temp = list_entry(head_children, struct task_struct, children);
+			
 		}
-		list_for_each(head_sibling, &task->sibling){
+		temp = NULL;
+		list_for_each(head_sibling, &current->sibling){
 			info.num_siblings++;
-			//task = list_entry(head_sibling, struct task_struct, sibling);
+			temp = list_entry(head_sibling, struct task_struct, sibling);
 		}
-
+		temp = NULL;
+		info.num_children = count_children;
+		info.num_siblings = count_sibling;
+		// printk("info.pid is %ld", info.pid);
+		// printk("info.state is %ld", info.state);
+		// printk("info.uid is %ld", info.uid);
+		// printk("info.nvcsw is %ld", info.nvcsw);
+		// printk("info.nivcsw is %ld", info.nivcsw);
+		// printk("info.num_children is %ld", info.num_children);
+		// printk("info.num_siblings is %ld", info.num_siblings);
+		// printk("================================================================");
+		filled++;
 		if (copy_to_user(&info_array[i], &info, sizeof(struct process_info)) != 0)
 		{
 			printk("Error copying info to user\n");
 			return -EFAULT;
 		}
-
-		filled++;
-
-		if (task->parent->pid == info.pid)
+		if (task->parent->pid == task->pid)
 		{
 			break;
 		}
 		task = task->parent;
-
 	}
+	printk("filled is %ld\n", filled);
 	if (copy_to_user(num_filled, &filled, sizeof(long)) != 0)
 	{
 		printk("Error copying filled to user\n");
 		return -EFAULT;
 	}
-	
 	return 0;
 }
